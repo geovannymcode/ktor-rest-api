c412449e67b9d16ff13927a3c36cc3f61a86ee21
Geovanny Mendoza <geovanny0401@gmail.com>
2023-03-16 21:02:54 -0500

    first commit

---
+++ src/main/kotlin/com/geovannycode/Application.kt
@@ -0,0 +1,13 @@
+package com.geovannycode
+
+import com.geovannycode.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
